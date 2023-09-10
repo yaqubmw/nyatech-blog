5b908f5a9cf4bec4de5cc98813b173ecb1fca42a
Yaqub Muflih Wiraatmaja <yaqubmw2@gmail.com>
2023-09-10 23:08:13 +0700

    fix: link in posts card

--- src/components/card/BlogCard.js
+++ src/components/card/BlogCard.js
@@ -16,50 +16,56 @@ async function BlogCard({ link, title, reactions, content, imageUrl, author }) {
   const authorData = await fetchAuthor({ authorId: author });
   return (
     <div className="blog-grid-container">
-      <div className="blog-grid-img">
-        {imageUrl ? (
-          <Image
-            sizes="100vw"
-            style={{
-              width: "100%",
-              height: "auto",
-            }}
-            src={imageUrl}
-            alt={title}
-            width={500}
-            height={300}
-          />
-        ) : (
-          <Image
-            sizes="100vw"
-            style={{
-              width: "100%",
-              height: "auto",
-            }}
-            src={defaultImageUrl}
-            alt="Default Image"
-            width={500}
-            height={300}
-          />
-        )}
-      </div>
-      <div className="blog-grid-section">
-        <Link href={`/blog/${link}`}>
+      <Link href={`/blog/${link}`}>
+        <div className="blog-grid-img">
+          {imageUrl ? (
+            <Image
+              sizes="100vw"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+              src={imageUrl}
+              alt={title}
+              width={500}
+              height={300}
+            />
+          ) : (
+            <Image
+              sizes="100vw"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+              src={defaultImageUrl}
+              alt="Default Image"
+              width={500}
+              height={300}
+            />
+          )}
+        </div>
+      </Link>
+      <Link href={`/blog/${link}`}>
+        <div className="blog-grid-section">
           <div className="blog-grid-title">{title}</div>
-        </Link>
-      </div>
-      <div className="blog-grid-section">
-        <p className="blog-grid-content">{trimmedContent}</p>
-      </div>
-      <div className="blog-grid-footer">
-        <p className="blog-grid-footer-content">Reactions: {reactions}</p>
-        <p className="blog-grid-footer-content">
-          Author:{" "}
-          <span className="font-medium">
-            {authorData.firstName} {authorData.lastName}
-          </span>
-        </p>
-      </div>
+        </div>
+      </Link>
+      <Link href={`/blog/${link}`}>
+        <div className="blog-grid-section">
+          <p className="blog-grid-content">{trimmedContent}</p>
+        </div>
+      </Link>
+      <Link href={`/blog/${link}`}>
+        <div className="blog-grid-footer">
+          <p className="blog-grid-footer-content">Reactions: {reactions}</p>
+          <p className="blog-grid-footer-content">
+            Author:{" "}
+            <span className="font-medium">
+              {authorData.firstName} {authorData.lastName}
+            </span>
+          </p>
+        </div>
+      </Link>
     </div>
   );
 }
